5ea7bda23ad5947ec1ca7231fcf8df5de68df743
Rasim Umut ÖZKURT <Rasim Umut ZKURT@DESKTOP-0F55LFS>
2021-05-29 15:12:03 +0300

    Add project files.

---
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -0,0 +1,16 @@
+﻿using Core.DataAccess.EntityFreamwork;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DataAccess.Concrete.EntityFreamwork.Context;
+
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfProductDal : EfEntityRepositoryBase<Product, NorthwindContext>, IProductDal
+    {
+
+    }
+}
